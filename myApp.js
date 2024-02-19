6f7643da759a86f1eae7846cb27e451a13195830
Rishbh Arora <vsryarora6879@gmail.com>
2024-02-19 15:22:51 +0530

    serve static

--- myApp.js
+++ myApp.js
@@ -1,15 +1,11 @@
 let express = require('express');
 let app = express();
 
-app.use(express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"))
 
 app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    res.sendFile(__dirname + "/views/index.html");
 })
-
-
-
-
 
 
 
