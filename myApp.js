eddd542117c3c2458bbb259b141b95ac517af1c6
Rishbh Arora <vsryarora6879@gmail.com>
2024-02-20 19:42:24 +0530

    added logger

--- myApp.js
+++ myApp.js
@@ -5,13 +5,19 @@ require('dotenv').config();
 
 app.use("/public", express.static(__dirname + "/public"))
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+} 
+
+app.use(logger);
+
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
 app.get("/json", (req,res) => {
     const text = "Hello json";
-    console.log(process.env.MESSAGE_STYLE);
     const message = process.env.MESSAGE_STYLE == "uppercase" ? text.toUpperCase() : text;
     res.json({
         "message": message
